f06ee4c7691f70a9157a0d8804ef359ee1b49d42
rishabhknowss <rishabhrai1515@gmail.com>
2024-03-22 15:54:34 +0530

    database init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string;
+  }
+}>()
+
+app.post('/api/v1/user/signup', async (c) => {
+
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  
+})
+
+app.get('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog' , (c)=>{
+  return c.text('hello hono')
+})
+export default app
